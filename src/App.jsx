0757c47c94c712e5bca22c7ec045a4fcb7efe7e0
Mughammad <caseameen44@gmail.com>
2026-06-06 01:17:45 +0200

    Fetch and display podcast data
    - Fetch podcast data on application load
    - Add loading and error state handling
    - Render podcast cards from API response
    - Display empty state when no podcasts are available

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,68 @@
+import { useEffect, useState } from "react";
+import { fetchPodcasts } from "./api/fetchPodcasts";
 import PodcastCard from "./components/PodcastCard";
 
-const testPodcast = {
-  id: "1",
-  title: "Placeholder Podcast Card", // placeholder title for testing
-  description:
-    "Something Was Wrong is an Iris Award-winning true-crime docuseries about the discovery, trauma, and recovery from shocking life events and abusive relationships.",
-  seasons: 3,
-  image:
-    "https://content.production.cdn.art19.com/images/cc/e5/0a/08/cce50a08-d77d-490e-8c68-17725541b0ca/9dcebd4019d57b9551799479fa226e2a79026be5e2743c7aef19eac53532a29d66954da6e8dbdda8219b059a59c0abe6dba6049892b10dfb2f25ed90d6fe8d9a.jpeg",
-  genres: [1, 2],
-  updated: "2022-11-03T07:00:00.000Z", // ISO date string
-};
-
+/**
+ * The main application component.
+ *
+ * Fetches podcast data  and renders loading, error, and podcast states.
+ *
+ * @returns {JSX.Element} application UI
+ */
 function App() {
-  return <PodcastCard podcast={testPodcast} />;
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadPodcasts() {
+      try {
+        const data = await fetchPodcasts();
+        setPodcasts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadPodcasts();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="message-container">
+        <div className="spinner"></div> <p>Loading podcasts...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="message-container">
+        <div className="error">
+          Error occurred while trying to fetch podcasts: {error}
+        </div>
+      </div>
+    );
+  }
+
+  // Handle case if no podcasts are returned
+  if (!loading && podcasts.length === 0) {
+    return (
+      <div className="message-container">
+        <p>No podcasts available.</p>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {podcasts.map((podcast) => (
+        <PodcastCard key={podcast.id} podcast={podcast} />
+      ))}
+    </main>
+  );
 }
 
 export default App;
